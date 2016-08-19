11b4bacffe76e95bcbe166269ca9966e1931091f
rayhatron <rayhatron@gmail.com>
2016-08-19 11:34:34 +0200

    Added click function for buttons

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 angular.module("ngPomodoro", []);
+
+(function()
+{
+  "use strict";
+  angular
+    .module("ngPomodoro")
+    .controller("ctrPomodoro", function($scope) {
+          $scope.session = 25;
+          $scope.break = 5;
+
+          $scope.increaseSession = function(){
+            $scope.session ++;
+          }
+
+          $scope.decreaseSession = function(){
+            if($scope.session == 1){
+              alert("Session cannot be less than 1");
+              return;
+            }
+            $scope.session --;
+          }
+
+          $scope.increaseBreak = function(){
+            $scope.break ++;
+          }
+
+          $scope.decreaseBreak = function(){
+            if($scope.break == 1){
+              alert("Break cannot be less than 1");
+              return;
+            }
+            $scope.break --;
+          }
+
+    });
+
+})();
