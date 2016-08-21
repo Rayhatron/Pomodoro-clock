b116f5a3676c35fb23c1a64427219a875928d41d
rayhatron <rayhatron@gmail.com>
2016-08-21 15:14:52 +0200

    Added timer start, end and display functions

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ angular.module("ngPomodoro", []);
   angular
     .module("ngPomodoro")
     .controller("ctrPomodoro", function($scope) {
-          $scope.session = 25;
+          $scope.session = 1;
           $scope.break = 5;
+          $scope.msTime;
+          $scope.timeout;
+          $scope.displayInterval;
+          $scope.displayTime = $scope.session;
+          $scope.hours;
+          $scope.minutes;
+          $scope.seconds;
 
           $scope.increaseSession = function(){
             $scope.session ++;
+            $scope.displayTime = $scope.session;
           }
 
           $scope.decreaseSession = function(){
@@ -19,6 +27,7 @@ angular.module("ngPomodoro", []);
               return;
             }
             $scope.session --;
+            $scope.displayTime = $scope.session;
           }
 
           $scope.increaseBreak = function(){
@@ -31,6 +40,34 @@ angular.module("ngPomodoro", []);
               return;
             }
             $scope.break --;
+          }
+
+          $scope.startTimer = function(){
+            $scope.finish();
+            console.log("Timer started.");
+            $scope.msTime = 60 * 1000 * $scope.session;
+            $scope.timeout = setTimeout($scope.finish, $scope.msTime);
+            $scope.displayInterval = setInterval($scope.updateDisplay, 1000);
+          }
+
+          $scope.finish = function(){
+            console.log("Done!!!!!");
+            clearTimeout($scope.timeout);
+            clearInterval($scope.displayInterval);
+            $scope.displayTime = $scope.session;
+          }
+
+          $scope.updateDisplay = function(){
+            console.log("Updating...");
+            console.log($scope.msTime);
+            $scope.msTime -= 1000;
+            $scope.seconds = Math.floor ( ($scope.msTime/1000) % 60);
+            $scope.minutes = Math.floor (($scope.msTime/1000/60) % 60);
+            $scope.hours = Math.floor (($scope.msTime/(1000*60*60)) % 24);
+            $scope.displayTime = $scope.hours + " : " + $scope.minutes + " : " + $scope.seconds;
+            document.getElementById('Time').innerHTML = $scope.displayTime;
+            console.log("Seconds left: " + $scope.seconds);
+
           }
 
     });
