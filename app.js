655697bd8db7e6e0d91ee4465a3644e64e4540cb
rayhatron <rayhatron@gmail.com>
2016-08-24 17:31:02 +0200

    Added auto start break countdown function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ angular.module("ngPomodoro", []);
   angular
     .module("ngPomodoro")
     .controller("ctrPomodoro", function($scope) {
+          $scope.title = "Session";
           $scope.session = 1;
           $scope.break = 5;
           $scope.msTime;
@@ -15,6 +16,8 @@ angular.module("ngPomodoro", []);
           $scope.hours;
           $scope.minutes;
           $scope.seconds;
+          $scope.bgPercentage = 1;
+          $scope.bgcolor;
 
           $scope.increaseSession = function(){
             $scope.session ++;
@@ -43,10 +46,23 @@ angular.module("ngPomodoro", []);
           }
 
           $scope.startTimer = function(){
-            $scope.finish();
+            $scope.bgPercentage = 99;
+            $scope.bgcolor = "green";
             console.log("Timer started.");
+            $scope.title = "Session";
             $scope.msTime = 60 * 1000 * $scope.session;
             $scope.timeout = setTimeout($scope.finish, $scope.msTime);
+            $scope.displayInterval = setInterval($scope.updateDisplay, 1000);
+            document.getElementById('clock').style.background =  "linear-gradient(360deg, " + $scope.bgcolor + " " + $scope.bgPercentage +  "%, transparent 1%)";
+          }
+
+          $scope.startBreak = function(){
+            console.log("Break started.");
+            $scope.title = "Break";
+            $scope.bgcolor = "red";
+            document.getElementById('title').innerHTML = $scope.title;
+            $scope.msTime = 60 * 1000 * $scope.break;
+            $scope.timeout = setTimeout($scope.finishAll, $scope.msTime);
             $scope.displayInterval = setInterval($scope.updateDisplay, 1000);
           }
 
@@ -55,19 +71,30 @@ angular.module("ngPomodoro", []);
             clearTimeout($scope.timeout);
             clearInterval($scope.displayInterval);
             $scope.displayTime = $scope.session;
+            $scope.startBreak();
+            document.getElementById('clock').style.background = "black";
+          }
+
+          $scope.finishAll = function(){
+            console.log("Done!!!!!");
+            clearTimeout($scope.timeout);
+            clearInterval($scope.displayInterval);
+            $scope.displayTime = $scope.session;
+            $scope.title = "Session";
+            document.getElementById('title').innerHTML = $scope.title;
+            document.getElementById('Time').innerHTML = $scope.displayTime;
+            document.getElementById('clock').style.background = "black";
           }
 
           $scope.updateDisplay = function(){
-            console.log("Updating...");
-            console.log($scope.msTime);
             $scope.msTime -= 1000;
+            $scope.bgPercentage = ($scope.msTime/ ($scope.session * 60 * 1000)) * 100;
             $scope.seconds = Math.floor ( ($scope.msTime/1000) % 60);
             $scope.minutes = Math.floor (($scope.msTime/1000/60) % 60);
             $scope.hours = Math.floor (($scope.msTime/(1000*60*60)) % 24);
             $scope.displayTime = $scope.hours + " : " + $scope.minutes + " : " + $scope.seconds;
             document.getElementById('Time').innerHTML = $scope.displayTime;
-            console.log("Seconds left: " + $scope.seconds);
-
+            document.getElementById('clock').style.background =  "linear-gradient(360deg, " + $scope.bgcolor + " " + $scope.bgPercentage +  "%, transparent 1%)";
           }
 
     });
